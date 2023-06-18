2095a3e2c05c0473f4c83db6d2d22fafc07fa840
drios2023-ga <drios111-general@yahoo.com>
2023-06-18 17:13:05 -0500

    fixes to contact, links, projects, staff

--- models/staffmember.js
+++ models/staffmember.js
@@ -1,12 +1,13 @@
 const staffMembers = [
     {membername: 'David Rios'},
     {membername: 'Anish Mathew'},
-    {membername: 'Jason Horst'}
+    {membername: 'Jason Horst'},
+    {membername: 'Yulia Glushenko'},
+    {membername: 'Matt Huntington'}
 ];
 
 module.exports = {
     getAllStaffMembers
-    
   };
 
   function getAllStaffMembers() {
